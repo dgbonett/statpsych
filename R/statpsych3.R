6637ac2981fb7bd4c1c3fd442310e1e5180e3518
dgbonett <88499135+dgbonett@users.noreply.github.com>
2023-04-05 16:25:31 -0700

    Update statpsych3.R

--- R/statpsych3.R
+++ R/statpsych3.R
@@ -2735,7 +2735,53 @@ power.prop1 <- function(alpha, n, p, es) {
  colnames(out) <- "Power"
  return(out)
 }
-         
+
+          
+#  power.prop2 ================================================================
+#' Approximates the power of a two-sample proportion test for planned sample 
+#' sizes
+#'
+#'
+#' @description
+#' Computes the approximate power for a test of equal population proportions
+#' in a 2-group design for the planned sample sizes. This function requires 
+#' planning values for both proportions. Set the proportion planning values 
+#' to .5 for a conservatively low power estimate. The planning value for the 
+#' proportion difference could be set to the difference of the two proportion
+#' planning values or it could be set to a minimally interesting effect size.
+#'
+#'
+#' @param  alpha  alpha level for hypothesis test 
+#' @param  n1     planned sample size for group 1
+#' @param  n2     planned sample size for group 2
+#' @param  p1     planning value of proportion for group 1
+#' @param  p2     planning value of proportion for group 2
+#' @param  es     planning value of proportion difference
+#'
+#'
+#' @return
+#' Returns the approximate power of the test
+#'
+#'
+#' @examples
+#' power.prop2(.05, 60, 40, .5, .5, .2)
+#'
+#' # Should return:
+#' #          Power
+#' # [1,] 0.4998515
+#'
+#'
+#' @importFrom stats qnorm
+#' @export
+power.prop2 <- function(alpha, n1, n2, p1, p2, es) {
+ za <- qnorm(1 - alpha/2)
+ z <- abs(es)/sqrt(p1*(1 - p1)/n1 + p2*(1 - p2)/n2) - za
+ pow <- pnorm(z)
+ out <- matrix(pow, nrow = 1, ncol = 1)
+ colnames(out) <- "Power"
+ return(out)
+}
+
           
 # ======================== Miscellaneous =====================================
 #  iqv =======================================================================
