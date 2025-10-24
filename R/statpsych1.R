c2e3c8b4589da5b502495174070855c814f22f4f
dgbonett <88499135+dgbonett@users.noreply.github.com>
2025-10-24 12:58:01 -0700

    Update statpsych1.R

--- R/statpsych1.R
+++ R/statpsych1.R
@@ -439,6 +439,79 @@ ci.lc.mean.bs <- function(alpha, m, sd, n, v) {
 }
 
 
+#  ci.lc.mean.scheffe =========================================================
+#' Scheffe confidence interval for a linear contrast of means in a 
+#' between-subjects design
+#' 
+#'
+#' @description
+#' Computes a Scheffe confidence interval for a linear contrast of population
+#' means in a between-subjects design. A Scheffe p-value is computed for the
+#' test statistic. The Scheffe method assumes equal population variances. This
+#' function is useful in exploratory studies where the linear contrast of 
+#' means was not planned but was suggested by the pattern of sample means. Use 
+#' the ci.lc.mean.bs function with a Bonferroni adjusted alpha value to compute
+#' simultaneous confidence intervals for two or more planned linear contrasts
+#' of means.
+#'
+#'
+#' @param     alpha  	alpha level for 1-alpha confidence
+#' @param     m     	vector of estimated group means
+#' @param     sd    	vector of estimated group standard deviations
+#' @param     n     	vector of sample sizes
+#' @param     v     	vector of between-subjects contrast coefficients
+#' 
+#'
+#' @return 
+#' Returns a 2-row matrix. The columns are:
+#' * Estimate - estimated linear contrast
+#' * SE - standard error
+#' * t - t test statistic
+#' * df - degrees of freedom
+#' * p - two-sided Scheffe p-value
+#' * LL - lower limit of the Scheffe confidence interval
+#' * UL - upper limit of the Scheffe confidence interval
+#' 
+#' 
+#' @references
+#' \insertRef{Snedecor1980}{statpsych}
+#'
+#'
+#' @examples
+#'
+#' m <- c(33.5, 37.9, 38.0, 44.1)
+#' sd <- c(3.49, 3.84, 3.65, 4.98)
+#' n <- c(10, 10, 10, 10)
+#' v <- c(.5, .5, -.5, -.5)
+#' ci.lc.mean.scheffe(.05, m, sd, n, v)
+#'
+#' # Should return:
+#' #  Estimate       SE         t           p        LL        UL
+#' #     -5.35 1.275231 -4.195317 0.002276683 -9.089451 -1.610549
+#'
+#'
+#' @importFrom stats qt
+#' @importFrom stats pt
+#' @export
+ci.lc.mean.scheffe <- function(alpha, m, sd, n, v) {
+ est <- t(v)%*%m 
+ a <- length(m)
+ df1 <- a - 1
+ df2 <- sum(n) - a
+ v1 <- sum((n - 1)*sd^2)/df2
+ se <- sqrt(v1*t(v)%*%solve(diag(n))%*%v)
+ t <- est/se
+ p <- 1 - pf(t^2/df1, df1, df2)
+ crit <- sqrt(df1*qf(1 - alpha, df1, df2))
+ ll <- est - crit*se
+ ul <- est + crit*se
+ out <- t(c(est, se, t, p, ll, ul))
+ colnames(out) <- c("Estimate", "SE", "t", "p", "LL", "UL")
+ rownames(out) <- ""
+ return(out)
+}
+
+
 #  ci.tukey ===================================================================
 #' Tukey-Kramer confidence intervals for all pairwise mean differences in a
 #' between-subjects design
