338efec0f80e5a0672a7ad822957b885c7b2cd71
dgbonett <88499135+dgbonett@users.noreply.github.com>
2025-11-07 10:46:20 -0800

    Update statpsych2.R

--- R/statpsych2.R
+++ R/statpsych2.R
@@ -3094,6 +3094,78 @@ size.ci.cor.prior <- function(alpha1, alpha2, cor0, n0, w) {
 }
 
 
+#  size.ci.cronbach.prior =====================================================
+#' Sample size for a Cronbach reliability confidence interval using an 
+#' reliability estimate from a prior study
+#'
+#'                
+#' @description
+#' Computes the sample size required to estimate a Cronbach reliability with
+#' desired confidence interval precision in applications where an estimated
+#' Cronbach reliability from a prior study is available. The actual confidence
+#' interval width in the planned study will depend on the value of the
+#' estimated reliability in the planned study. An estimated Cronbach reliability
+#' from a prior study can be used to compute a lower prediction limit for the 
+#' estimated reliability in the planned study, which is then used as a planning
+#' value in the sample size analysis. Using a larger confidence level 
+#' (1 - alpha2) for the prediction interval will increase the probability
+#' that the width of the confidence interval in the planned study will be less 
+#' than or equal to the desired width.
+#'
+#' This sample size approach assumes that the population Cronbach reliability 
+#' that was estimated in the prior study is very similar to the population 
+#' Cronbach reliability that will be estimated in the planned study. If
+#' an estimated Cronbach reliability from a prior study is not available, the 
+#' researcher must use expert opinion to guess the value of the Cronbach 
+#' reliability that will be observed in the planned study. The 
+#' \link[statpsych]{size.ci.cronbach} function uses a reliability planning 
+#' value that is based on expert opinion regarding the likely value of the 
+#' reliability estimate that will be observed in the planned study.
+#'
+#'
+#' @param  alpha1  alpha level for 1-alpha1 confidence in the planned study
+#' @param  alpha2  alpha level for the 1-alpha2 prediction interval 
+#' @param  rel0    estimated reliability in prior study
+#' @param  n0      sample size in prior study
+#' @param  r       number of measurements (items, raters, forms)
+#' @param  w       desired confidence interval width
+#'
+#'
+#' @return
+#' Returns the required sample size
+#'
+#'
+#' @examples
+#' size.ci.cronbach.prior(.05, .10, .86, 50, 5, .15)
+#'
+#' # Should return:
+#' # Sample size
+#' #          71
+#'
+#'
+#' @importFrom stats qnorm
+#' @export                 
+size.ci.cronbach.prior <- function(alpha1, alpha2, rel0, n0, r, w) {
+ if (rel0 > .999 | rel0 < 0) {stop("reliability must be between 0 and .999")}
+ ci <- ci.cronbach(2*alpha2, rel0, r, n0)
+ ll <- ci[1,3]                                  
+ if (ll < 0) {rel = 0} else {rel <- ll}
+ n <- size.ci.cronbach(alpha1, rel, r, w)
+ pi <- pi.cronbach(alpha2, rel0, r, n0, n, type = 3)
+ ll <- pi[1,1]                                  
+ if (ll < 0) {rel = 0} else {rel <- ll}
+ n <- size.ci.cronbach(alpha1, rel, r, w)
+ pi <- pi.cronbach(alpha2, rel0, r, n0, n, type = 3)
+ ll <- pi[1,1]                                  
+ if (ll < 0) {rel = 0} else {rel <- ll}
+ n <- size.ci.cronbach(alpha1, rel, r, w)	 
+ out <- matrix(n, nrow = 1, ncol = 1)
+ colnames(out) <- "Sample size"
+ rownames(out) <- ""
+ return(out)
+}
+
+
 #  size.ci.ancova2 =========================================================
 #' Sample size for a 2-group ANCOVA confidence interval
 #'
