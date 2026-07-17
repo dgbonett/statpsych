0e2db8bc89cfd9f41c636a4ced34b774a52eef0e
dgbonett <88499135+dgbonett@users.noreply.github.com>
2026-07-17 10:46:01 -0700

    Update statpsych3.R

--- R/statpsych3.R
+++ R/statpsych3.R
@@ -3264,18 +3264,16 @@ size.ci.agree <- function(alpha, G, w) {
 #' proportion in the planned study. An estimated proportion from a prior study
 #' is used to predict the value of the estimated proportion in the planned 
 #' study, and the predicted proportion estimate is then used as a planning
-#' value in the sample size computation. The probability that the 1 - alpha1
-#' confidence interval in the planned study will have a width that is less
-#' than the desired width is approximately 1 - alpha2 where alpha1 and alpha2
-#' are specified values.
+#' value in the sample size computation. The probability that the 1-alpha1
+#' confidence interval for the populatin proportion in the planned study will
+#' have a width that is less than the desired width is approximately 1-alpha2
+#' where alpha1 and alpha2 are specified values. This sample size approach 
+#' assumes that the population proportion in the prior study is very similar
+#' to the population proportion in the planned study. 
 #'
-#' This sample size approach assumes that the population proportion in the 
-#' prior study is very similar to the population proportion in the planned 
-#' study. If an estimated proportion from a prior study is not available the 
-#' researcher must use expert opinion to guess the value of the proportion
-#' that will be observed in the planned study. The \link[statpsych]{size.ci.prop} 
-#' function uses a proportion planning value that is a subjective estimate
-#' of the proportion estimate that will be observed in the planned study.
+#' The similar \link[statpsych]{size.ci.prop} function uses a proportion
+#' planning value that is a subjective estimate of the proportion estimate 
+#' that will be observed in the planned study. 
 #'
 #' For more details, see Section 1.16 of Bonett (2021, Volume 3)
 #'
@@ -3285,7 +3283,6 @@ size.ci.agree <- function(alpha, G, w) {
 #' @param  p0      estimated proportion in prior study
 #' @param  n0      sample size in prior study
 #' @param  w       desired confidence interval width
-#'
 #'
 #'
 #' @return
